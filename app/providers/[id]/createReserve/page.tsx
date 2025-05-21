3864eaf933cd5ea09168f1298c374308821c5d50
Marco Mignacco <marcomignacco03@gmail.com>
2025-05-20 21:02:22 -0300

    refactor: update ProviderReserveProps to use pathParams and handle optional id

--- app/providers/[id]/createReserve/page.tsx
+++ app/providers/[id]/createReserve/page.tsx
@@ -5,19 +5,20 @@ import ReservationForm from "@/components/agreement/create-reserve-form"
 
 
 type ProviderReserveProps = {
-  params: {
-    id: string
-  }
+  pathParams: Promise<{
+    id?: string
+  }>
 }
 
-export default async function Page({ params }: ProviderReserveProps) {
+export default async function Page({ pathParams }: ProviderReserveProps) {
+  const params = await pathParams
   const provinces = await getProvinces()
   const provider = await getProviderInfo(Number(params.id))
 
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-6">Reserva tu servicio de cuidado</h1>
-      <ReservationForm id={params.id} provinces={provinces} providerName={`${provider.usuario.nombre} ${provider.usuario.apellido}`} />
+      <ReservationForm id={params.id ?? ""} provinces={provinces} providerName={`${provider.usuario.nombre} ${provider.usuario.apellido}`} />
     </div>
   )
 }
